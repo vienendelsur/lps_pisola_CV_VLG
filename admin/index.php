41ec369c4865f6a181f0c583a5b9e2dd4b6c6ce2
Patrick Isola <isola.patrick@gmail.com>
2017-10-06 16:23:07 +0200

    commit 7
    commit 7

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="utf-8">
 <?php
-		$sql = $pdoCV->query(" SELECT * FROM t_utilisateurs ");
+		$sql = $pdoCV->query(" SELECT * FROM t_utilisateurs "); 
 		$ligne_utilisateur = $sql->fetch();
 	?>
 <title>Admin : <?php echo($ligne_utilisateur['pseudo']); ?></title>
