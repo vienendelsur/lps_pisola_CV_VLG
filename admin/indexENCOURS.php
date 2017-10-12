48053f1a0554cb8a6f22601320a6a1af540e8518
Patrick Isola <isola.patrick@gmail.com>
2017-10-12 12:15:33 +0200

    Commit  12
    Commit 12 Bootstrap

---
+++ admin/indexENCOURS.php
@@ -0,0 +1,17 @@
+<?php require 'connexion.php'; ?>
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<?php
+		$sql = $pdoCV->query(" SELECT * FROM t_utilisateurs "); 
+		$ligne_utilisateur = $sql->fetch();
+	?>
+<title>Admin : <?php echo($ligne_utilisateur['pseudo']); ?></title>
+</head>
+
+<body>
+<h1>Admin du site cv de <?php echo($ligne_utilisateur['prenom']); ?></h1>
+<p>texte</p>
+</body>
+</html>
