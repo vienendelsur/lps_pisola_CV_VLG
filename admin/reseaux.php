4cba71b9d37c7d25def2aa256ac5d5f49371eff9
Patrick Isola <isola.patrick@gmail.com>
2017-12-12 12:40:47 +0100

    c 58
    c 58

--- admin/reseaux.php
+++ admin/reseaux.php
@@ -1,4 +1,31 @@
-<?php require 'connexion.php'; ?>
+<?php require 'connexion.php'; 
+
+session_start();// à mettre dans toutes les pages de l'admin
+	if(isset($_SESSION['connexion']) && $_SESSION['connexion']=='connecté'){//on établit que la variable de session est passée et contient bien le terme "connexion" 
+		$id_utilisateur=$_SESSION['id_utilisateur'];
+		$prenom=$_SESSION['prenom'];		
+		$nom=$_SESSION['nom'];
+		
+		//echo $_SESSION['connexion'];		
+        //var_dump($_SESSION);
+	}else{//l'utilisateur n'est pas connecté
+		header('location: login.php');		
+}//ferme le else du if isset
+
+//pour se déconnecter de l'admin à mettre dans toutes les pages ??? ou juste sur la page login.php ?
+if(isset($_GET['quitter'])){//on récupère le terme quitter dans l'url 
+	
+	$_SESSION['connexion']='';//on vide les variables de session
+	$_SESSION['id_utilisateur']='';
+	$_SESSION['prenom']='';
+	$_SESSION['nom']='';
+		
+		unset($_SESSION['connexion']);
+		session_destroy();
+	header('location:../index.php');	
+}//ferme le if isset de la déconnexion
+
+?>
 <?php
 // gestion des contenus de la BDD compétences
 //insertion d'une compétence
